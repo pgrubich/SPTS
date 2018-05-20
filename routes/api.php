049ac6544bd9fc400caaf7ecd98df0f73f44f90c
lukaszszy <lookszym@gmail.com>
2018-05-20 12:50:27 +0200

    Read dyscyplines.json from storage

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,8 @@ Route::get('profile/{id}', function($id){
 //Route::resource('profiles','TrainersController');
 
 
+Route::get('api/dyscyplines.json','DisciplinesList@show');
+
 Route::get('api/profiles/{id}','TrainersController@show');
 
 Route::get('api/dyscyplina/{id}','ListByDisciplineController@show');
