6388a524dab2aa389decb3539d4e60e60cf4de6c
mrtubisz <mr.tubisz@gmail.com>
2018-05-28 12:52:56 +0200

    go to trainer profile after searching

--- public/js/profile/profilesResult.js
+++ public/js/profile/profilesResult.js
@@ -15,18 +15,26 @@ xhr.onload = function() {
     responseObject = JSON.parse(xhr.responseText);
     var content = '';
     for(var i = 0; i < responseObject.length; i++) {
-      content += '<div class="single-trainer-result"><div class="profile-picture"></div><div class="profile-info">';
+      content += '<div class="single-trainer-result" id="trainer_record_';
+      content += responseObject[i].id+'"><div class="profile-picture">';
+      content += '</div><div class="profile-info">'
       content += "<p>"+ responseObject[i].name + " " + responseObject[i].surname +"</p>";
       content += "<p>"+ responseObject[i].tr_disc[i].discipline_name +"</p>";
       content += "<p> Miejsca: "+ responseObject[i].tr_pl[i].place+"</p>";
       content += "<p> Opis: "+ responseObject[i].description+"</p>";
       content += '</div><div style="clear:both;"></div></div>';
+      
     }
     var trainerBox = document.getElementById("trainers-box");
     trainerBox.innerHTML = content;
-
+    for(var i = 0; i < responseObject.length; i++){
+      var event = document.getElementById("trainer_record_"+responseObject[i].id);
+      event.addEventListener('click',function(){ window.location.href = 'http://pri.me/profiles/'+this.id.slice(-1);},false);
+    }
+    
+    
   }
 };
 
- xhr.open('GET', 'http://pri.me/api/'+discipline+'/'+city, true);        
+xhr.open('GET', 'http://pri.me/api/'+discipline+'/'+city, true);        
 xhr.send(null);                                 
