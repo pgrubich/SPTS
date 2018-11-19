51d1f652b1613afdd842f5b439f078676c713d69
lukaszszy <lookszym@gmail.com>
2018-11-19 21:30:20 +0100

    Delete photo id

--- routes/web.php
+++ routes/web.php
@@ -52,13 +52,13 @@ Route::post('editTrainerOffer', ['uses' =>'editProfileController@editTrainerOffe
 
 Route::post('destroyOffer', ['uses' =>'editProfileController@destroyOffer']);
 
+Route::post('/destroy/{id}', ['uses' => 'PhotosController@destroy']);
+
 Route::post('editEmailInfo', ['uses' =>'editProfileController@updateEmailInfo']);
 
 Route::post('editPasswordInfo', ['uses' =>'editProfileController@editPasswordInfo']);
 
 Route::post('store', ['uses' => 'PhotosController@store']);
-
-Route::post('destroy', ['uses' => 'PhotosController@destroy']);
 
 Route::post('orderTraining', ['uses' =>'CalendarController@orderTraining']);
 
