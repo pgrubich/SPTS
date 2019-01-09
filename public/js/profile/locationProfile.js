9dabbb979c5254a2e2780f5e6fdf6caa1d04c821
Marcin Tubisz <mr.tubisz@gmail.com>
2019-01-09 11:45:46 +0100

    gallery fix

---
+++ public/js/profile/locationProfile.js
@@ -0,0 +1,12 @@
+// function initMap() {
+
+
+//     var options = {
+//         zoom: 12,
+//         center: {lat: 52.4069200, lng: 16.9299300}
+//     }
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), options);
+    
+//     // var marker = new google.maps.Marker({position: city, map: map});
+// }
