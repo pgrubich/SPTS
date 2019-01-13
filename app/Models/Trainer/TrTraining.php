4a61618377b09a4950fb9f188727603ec1c9c5e3
lukaszszy <lookszym@gmail.com>
2019-01-13 15:07:20 +0100

    Extra calendar atributes

--- app/Models/Trainer/TrTraining.php
+++ app/Models/Trainer/TrTraining.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class TrTraining extends Model
 {
     protected $table = 'trainers_trainings_dates';
+    protected $appends = ['full_begin_date', 'full_end_date'];
 
     public function trainer(){
         return $this->belongsTo('Trainer');
@@ -14,6 +15,16 @@ class TrTraining extends Model
 
     public function trOrdTr(){
         return $this->hasMany('App\Models\Trainer\TrOrderedTrainings', 'training_id');
+    }
+
+    public function getFullBeginDateAttribute()
+    {
+        return $this->date.(' ').($this->begin_time);
+    }
+
+    public function getFullEndDateAttribute()
+    {
+        return $this->date.(' ').($this->end_time);
     }
 
 }
